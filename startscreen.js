3fbd7c0f69bf6055e343566a68536e98d2395450
Patrick Wu <scorpio7500@gmail.com>
2016-10-08 21:34:12 -0400

    minor fixes in startscreen allowing for exporting

--- startscreen.js
+++ startscreen.js
@@ -4,23 +4,19 @@
 import React, { Component } from 'react'
 import { AppRegistry, Image, View } from 'react-native'
 
-class StartScreen extends Component {
-  render() {
-    let pic = {
-      uri: 'http://images.clipartpanda.com/school-bus-driver-quotes-5047_school_bus_with_happy_children.png'
-    };
-    return(
-
-      <View style = {{
-
-        flex: 1,
-        flexDirection: 'column'
-        justifyContent: 'center'
-        alightItems: 'center'
-      }}>
-      <Image source = {pic} /> <VIEW />
-      );
-  }
-};
-
-AppRegistry.registerComponent('StartScreen', () => StartScreen)
+export default class StartScreen extends Component {
+	render() {
+		const pic = {
+			uri: 'http://images.clipartpanda.com/school-bus-driver-quotes-5047_school_bus_with_happy_children.png'
+		};
+		return(
+				<View style = {{	
+					flex: 1,
+					flexDirection: 'column'
+					justifyContent: 'center'
+					alighnItems: 'center'
+					}}>
+				<Image source = {pic} /> 
+				</View>
+			}
+		};
